57467072da41ac50a041c941a34d7f47e3bbffda
osmarsalesjr <osmarsalesjr@gmail.com>
2017-08-04 21:20:23 -0300

    Alteracoes em 1273 justificador (presentation error)

--- justificador.py
+++ justificador.py
@@ -1,43 +1,47 @@
 
 def main():
+
+    results = list()
     while True:
-        n = int(input())
+
+        nWords = int(input())
         
-        if n <= 0:
+        if (nWords <= 0):
+
+            results.pop()
             break
         else:
-            words = []
+
+            words = list()
+
+            for i in range(nWords):
+
+                word = input()
+                words.append(word.upper())
             
-            for i in range(n):
-                wrd = input()
-                words.append(wrd.upper())
-            
-            size_max = 0
-            for w in words:
-                if len(w) > size_max:
-                    size_max = len(w)
-                
-            rlts = []
-            for i in range(len(words)):
-                d = size_max - len(words[i])
-                if d <= 0:
-                    palavra = words[i]
-                else:
-                    spcs = " " * d
-                    palavra = spcs + words[i]
-                    
-                rlts.append(palavra)
-            
-            listar(rlts)
-            
+            size_max = len(sorted(words, key = len).pop())
+
+            for word in words:
+
+                sizeWord = size_max - len(word)
+                spaces = ""
+
+                for i in range(sizeWord):
+                    spaces += " "
+
+                results.append(spaces + word)
+
+            results.append("")
+
+
+    listar(results)
 
 
 def listar(lista):
-    for i in lista:
-        print(i)
-    
-    print("")
 
+    for i in lista:
+
+        print(i)
 
 
 if __name__ == "__main__":
