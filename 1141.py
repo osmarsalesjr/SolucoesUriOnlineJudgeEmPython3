ad0347f329b9abedcb64bcefb4ba99db289f314d
osmarsalesjr <osmarsalesjr@gmail.com>
2017-08-07 13:52:42 -0300

    Tempo excedido

--- 1141.py
+++ 1141.py
@@ -1,40 +1,40 @@
 
 
 def main():
-	while True:
-		n = int(input())
+    while True:
+        n = int(input())
 
-		if n <= 0:
-			break
+        if n <= 0:
+            break
 
-		contadores = []
-		wds = []
+        contadores = []
+        wds = []
 
-		for i in range(n):
-			wds.append(input())
+        for i in range(n):
+            wds.append(input())
 
-		wds.sort(key = len)
-		size = len(wds)
+        wds.sort(key = len)
+        size = len(wds)
 
-		for j in range(size):
-			word = wds[j]
-			contador = 1
+        for j in range(size):
+            word = wds[j]
+            contador = 1
 
-			for i in range(j + 1, size):
-				if word in wds[i]:
-					contador += 1
-					word = wds[i]
+            for i in range(j + 1, size):
+                if word in wds[i]:
+                    contador += 1
+                    word = wds[i]
 
-			contadores.append(contador)
+            contadores.append(contador)
 
-			maior = max(contadores)
-			if maior > (size - (j + 1)):
-				break
+            maior = max(contadores)
+            if maior > (size - (j + 1)):
+                break
 
-		print(maior)
+        print(maior)
 
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
