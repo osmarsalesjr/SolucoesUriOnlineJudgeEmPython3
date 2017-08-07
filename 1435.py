d75ccd1271898d206b4ac074d6a1385e79b8bf83
osmarsalesjr <osmarsalesjr@gmail.com>
2017-08-07 13:37:18 -0300

    Presentation eror

--- 1435.py
+++ 1435.py
@@ -1,26 +1,24 @@
 def main():
-
     matrizes = []
-    
-    while(True):
-        
+
+    while (True):
+
         numero = int(input())
-        
+
         if (numero <= 0):
             break
 
         matriz = gerarMatriz(numero)
 
-        if(numero > 2):
+        if (numero > 2):
             reformularMatriz(matriz, numero)
-        
+
         matrizes.append(matriz)
 
     imprimir(matrizes)
 
 
 def gerarMatriz(numero):
-
     matriz = []
 
     for i in range(numero):
@@ -30,11 +28,10 @@ def gerarMatriz(numero):
 
 
 def reformularMatriz(matriz, numero):
-
     if (numero % 2 == 0):
-        limite = int(numero/2) + 1
+        limite = int(numero / 2) + 1
     else:
-        limite = int(numero/2) + 2
+        limite = int(numero / 2) + 2
 
     inicio = 1
     fim = numero - 1
@@ -49,16 +46,10 @@ def reformularMatriz(matriz, numero):
 
 
 def imprimir(matrizes):
-
-    for i in range(len(matrizes) - 1):
+    for i in range(len(matrizes)):
         for j in range(len(matrizes[i])):
-            print(" ".join([str(z) for z in matrizes[i][j]]))
+            print(("  ") + "   ".join([str(z) for z in matrizes[i][j]]))
         print("")
-
-    listas = matrizes[len(matrizes) - 1]
-    for lista in listas:
-        print(" ".join([str(z) for z in lista]))
-
 
 
 if __name__ == "__main__":
